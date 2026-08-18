7e797adae5e6718b81d4d81fa4a4e97b7bdbb8e1
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-08-18 18:18:13 +0530

    finished login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,34 @@ import userModel from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+}
+
+async function loginUser(req, res) {
+    const { userid, password } = req.body;
+    const user = await userModel.findOne({$or: [ {email: userid}, {username: userid}] });
+    if (!user) {
+        return res.error(400).json({message: "User not found"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.error(400).json({message: "Invalid password"});
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN || "1d"
+    });
+
+    res.cookie("token", token, options);
+
+    return res.status(200).json({ message: "Login successful", token });
+}
+
 async function registerUser(req, res) {
     const { username, email, password } = req.body;
 
@@ -26,7 +54,9 @@ async function registerUser(req, res) {
         maxAge: 1000 * 60 * 60 * 24 // 1 day
     });
 
-    res.status(201).json({ message: "User registered successfully", token });
+    newUser.password = undefined; // Remove password from the response
+
+    res.status(201).json({ message: "User registered successfully", newUser, token });
 }
 
 export { registerUser };
