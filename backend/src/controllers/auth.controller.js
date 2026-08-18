5b05f8650bc2a177c63f7c4396ddfaa89db5add4
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-08-18 18:42:42 +0530

    tested and debugged auth controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -13,12 +13,12 @@ async function loginUser(req, res) {
     const { userid, password } = req.body;
     const user = await userModel.findOne({$or: [ {email: userid}, {username: userid}] });
     if (!user) {
-        return res.error(400).json({message: "User not found"});
+        return res.status(400).json({message: "User not found"});
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-        return res.error(400).json({message: "Invalid password"});
+        return res.status(400).json({message: "Invalid password"});
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
@@ -45,6 +45,7 @@ async function registerUser(req, res) {
     const hash = await bcrypt.hash(password, 10);
     const newUser = new userModel({ username, email, password: hash });
 
+    await newUser.save();
     const token  = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
 
     res.cookie("token", token, {
