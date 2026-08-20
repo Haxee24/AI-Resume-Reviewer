7586d804affbbe29cb7dc14e9867ae41f81bb533
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-08-20 17:04:06 +0530

    created auth middleware

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import {registerUser, loginUser} from '../controllers/auth.controller.js';
+import {registerUser, logoutUser, getUser, loginUser} from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
+authRouter.get('/logout', logoutUser);
+authRouter.get("/get-me", getUser);
 
 export default authRouter;
