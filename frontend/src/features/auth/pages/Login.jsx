58682c20aa457f435b24e9af7298c8996819b42b
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-08-24 17:42:15 +0530

    added cross links to login and register pages

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,12 +1,18 @@
 import '../auth.form.scss';
+import {Link} from "react-router";
 export default function Login() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <main>
             <div className="form-container">
                 <h1>
                     Login
                 </h1>
-            <form action="">
+            <form action="POST" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="email">Email or Username</label>
                     <input type="text" name="userid" id="email" placeholder="Enter your email or username" />
@@ -16,6 +22,7 @@ export default function Login() {
                     <input type="password" name="password" id="password" placeholder="Enter your password" />
                 </div>
                 <button className="button primary-button" type="submit">Login</button>
+                <p>Don't have an account? <Link to="/register">Sign up</Link></p>
             </form>
             </div>
         </main>
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,5 +1,34 @@
+import '../auth.form.scss';
+import {Link} from "react-router";
 export default function Register() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <h1>Register</h1>
+        <main>
+            <div className="form-container">
+                <h1>
+                    Register
+                </h1>
+            <form action="POST" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Enter your email or username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Username</label>
+                    <input type="text" name="username" id="usernname" placeholder="Enter your email or username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter your password" />
+                </div>
+                <button className="button primary-button" type="submit">Register</button>
+                <p>Already have an account? <Link to="/login">Sign in</Link></p>
+            </form>
+            </div>
+        </main>
     )
 }
