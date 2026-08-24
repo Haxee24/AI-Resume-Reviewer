ef4c6697bb428983a1e0165a4ccb556a1cc743ff
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-08-24 17:21:59 +0530

    added scss styling to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import {RouterProvider} from "react-router";
 import router from "./app.routes.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <RouterProvider router={router} />
   )
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,5 +1,23 @@
+import '../auth.form.scss';
 export default function Login() {
     return (
-        <h1>Login</h1>
+        <main>
+            <div className="form-container">
+                <h1>
+                    Login
+                </h1>
+            <form action="">
+                <div className="form-group">
+                    <label htmlFor="email">Email or Username</label>
+                    <input type="text" name="userid" id="email" placeholder="Enter your email or username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter your password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            </div>
+        </main>
     )
 }
